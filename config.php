526fe9d2dbab075460f9b249074e9e2b7b7c772f
Ward Mundy & Associates LLC <wardmundy@users.noreply.github.com>
2017-01-05 13:49:07 -0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+// only change the next two entries if Tesla updates its API access credentials
+$tesla_client_id = "e4a9949fcfa04068f59abb5a658f2bac0a3428e4652315490b659d5ab3f35a9e";
+$tesla_client_secret = "c75f14bbadc8bee3a7594412c31416f8300256d7668ea7e6e7f06727bfb9d220";
+
+// insert your credentials for access to your teslamotors.com account below
+$tesla_email="yourname@yourdomain.com";
+$tesla_password="yourpassword";
+
+// specify the PIN to be used for Valet Mode with your vehicle
+$valet_pin=1234;
+
+// specify the default temperature settings for your vehicle in Centigrade degrees (22C = 71.6F)
+$driver_temp=22.0;
+$passenger_temp=22.0;
+?>
