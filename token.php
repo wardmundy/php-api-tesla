6c9e3c807065329f1dd40184aedffe3537ff94b3
Ward Mundy & Associates LLC <wardmundy@users.noreply.github.com>
2017-01-05 13:53:59 -0500

    Add files via upload

---
+++ token.php
@@ -0,0 +1,35 @@
+#!/usr/bin/php -q
+<?php
+
+include_once("config.php");
+
+// Get cURL resource
+$curl = curl_init();
+
+$params = array(
+        'grant_type' => 'password',
+        'client_id' => $tesla_client_id,
+        'client_secret' => $tesla_client_secret,
+        'email' => $tesla_email,
+        'password' => $tesla_password,
+);
+
+var_dump($params);
+
+curl_setopt($curl, CURLOPT_URL, "https://owner-api.teslamotors.com/oauth/token");
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($curl, CURLOPT_HEADER, FALSE);
+curl_setopt($curl, CURLOPT_POST, TRUE);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
+
+// Send the request & save response to $resp
+$resp = curl_exec($curl);
+// Close request to clear up some resources
+curl_close($curl);
+
+file_put_contents("token",$resp);
+echo "Done. Tokens written to file: token";
+echo $resp;
+echo "";
+echo "";
+?>
